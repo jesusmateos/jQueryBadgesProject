38a025da65d277347c4c219d1967d5702c967602
Jesus Mateos <jesusmateos@gmail.com>
2017-11-18 18:23:52 +0100

    Add logic to show badges

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,23 @@
 $(function() {
 
   // your code will go here
+  var $badges = $('#badges');
+
+  $.ajax({
+    url: 'https://www.codeschool.com/users/yisus.json',
+    dataType: 'jsonp',
+    success: function(resp){
+      var html = ''
+      resp.courses.completed.forEach(element => {
+        html += '<div class="course">';
+        html += '<h3>' + element.title + '</h3>';
+        html += '<img src="' + element.badge + '" />';
+        html += '<a href="' + element.url + '" target="_blank" class="btn btn-primary">See Course</a>';
+        html += '</div>';
+      });
+
+      $badges.html(html);
+    }
+  });
 
 });
